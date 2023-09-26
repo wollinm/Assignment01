0a3e81b47f7dfc9c0d901367fb6ec1bfd16a968d
Magdalena Wollin <mwollino@hotmail.com>
2023-09-26 01:15:12 -0700

    completed up to alert boxes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+//Start Assignment 01 
+document.write(`<h1>Start Assignment 01</h1>`);
 /* JavaScript
 Author: Magdalena Wollin
 Date: 09/25/2023
 Assignment 01
 */
+// Variables
+let myName = 'this is my name'
+let myClass = "here"
 //Exercise 1
 //1. Convert the following highlighted identifiers to Camel Case notation:
 //   let some_month
@@ -10,17 +15,21 @@ Assignment 01
 //   let current-month
 //   let summer_month
 //   let MyLibrary-function
-let someMonth
-functionTheMonth()
-let currentMonth
-let summerMonth
-let myLibraryFunction
+let someMonth;
+function theMonth() {}
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
 
 //2. Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
 let nLiteral = 6;
 let sLiteral = 'Hello';
 let bLiteral = true;
 let nullLiteral = '';
+document.write(`The nLiteral is ${nLiteral}<br/>`);
+document.write(`The sLiteral is ${sLiteral}<br/>`);
+document.write(`The bLiteral is ${bLiteral}<br/>`);
+document.write(`The nullLiteral is ${nullLiteral}<br/>`);
 
 //3. Give me two examples of complex / variable expressions.
 
@@ -39,26 +48,63 @@ let bMayWeContactYou;
 
 //5. Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
 strFirstName = 'Molly';
+document.write(`The strFirstName is ${strFirstName}<br/>`);
 intYourAge = 60;
-bMayWeContactYou = True;
-document.write(`Your name is ${strFirstName} `);
+document.write(`The intYourAge is ${intYourAge.toString(10)}<br/>`);
+bMayWeContactYou = 1;
+document.write(`May we contact you? ` + Boolean(bMayWeContactYou) + `<br/>`);
 
 
 //6. Create a variable.
 //   Add a number and a string and display the coerced result in the browser’s console window.
+let intvar = 5;
+let strvar = "the number is     ";
+document.write(strvar + intvar + `<br/>`);
+//console.log("coerced result");
+//console.log(strvar + intvar);
 
 //7. Create two variables.
 //   For the first variable, add a Boolean and a string and display the coerced result.
-// For the second variable, add a number and a Boolean and display the coerced result.
+//   For the second variable, add a number and a Boolean and display the coerced result.
+let bval = 1;
+let sval = "boolean and a string ";
+let ival = 5;
+document.write(`${sval}` + Boolean(bMayWeContactYou) + `<br/>`);
+document.write(`${ival}` + bMayWeContactYou + `<br/>`);
 
-//8. Is the following string literal valid? If not, how would you fix it?
+//8. Is the following string literal valid?  NO 
+//   If not, how would you fix it?  Need to add escape char \ from I'm to I\'m
 //   let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'
 //   console.log(someString)
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+document.write(someString);
+console.log(someString);
 
 //9. Create a variable that produces a null value in the console window.
 //   Then, create a variable that produces an undefined value in the console window.
 
-//10. Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+/*
+let x='';
+let x = typeof myVar
+/*
+
+//10. Use the unary typeof operator on various literals to return the following types within 
+//    the console window: string, number, Boolean, object, and undefined.
+
+/*  Console output
+"'Molly' is " + typeof "Molly"
+"'Molly' is string"
+"'Molly' is " + typeof "Molly"
+"'Molly' is string"
+"3.14 is " + typeof 3.14
+'3.14 is number'
+"true is " + typeof true
+'true is boolean'
+"{name:'Molly', age:50} is " + typeof {name:'Molly', age:50}
+"{name:'Molly', age:50} is object"
+"myVar is " + typeof myVar
+'myVar is undefined'
+*/
 
 //11. Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows:
 
@@ -66,6 +112,9 @@ document.write(`Your name is ${strFirstName} `);
 
 //    Substitute my name for your name. Although not necessary in practice, I want you to use 2 concatenation operators to construct this string of text. 
 //    One after the text “Hello” and a second one after your name and before the comma.
+
+//<!--<body onload="javascript:alert(['Hello', ' ', 'Magdalena Wollin'].join('') + ', welcome to the JavaScript class!')" > -->
+
 
 //12. Declare a variable called name and set it equal to your name.
 //    Substitute your name in the previous alert string with the variable instead.
@@ -75,6 +124,8 @@ document.write(`Your name is ${strFirstName} `);
 //
 //    Hello Zak Ruvalcaba.
 //    Welcome to the JavaScript class!
+//
+//  <body onload="javascript:alert('Hello Magdalena Wollin\n'+'Welcome to the JavaScript class!')" >
 
 //15.Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable.
 //16.Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
